519a25113e387df2ed7866d1d097e61e75b7bf04
Jan Sterba <info@jansterba.com>
2020-01-22 10:52:19 +0100

    more logging

--- src/c_api/c_api_error.h
+++ src/c_api/c_api_error.h
@@ -16,7 +16,7 @@
 /*! \brief every function starts with API_BEGIN();
      and finishes with API_END() or API_END_HANDLE_ERROR */
 #define API_END() } catch(dmlc::Error &_except_) { return XGBAPIHandleException(_except_); } \
-catch (...) { std::cout << "EXCEPTION CAUGHT" <<  << "\n"; return 1; } \
+catch (...) { std::cout << "EXCEPTION CAUGHT" << "\n"; return 1; } \
 return 0;  // NOLINT(*)
 #define CHECK_HANDLE() if (handle == nullptr) \
   LOG(FATAL) << "DMatrix/Booster has not been intialized or has already been disposed.";
